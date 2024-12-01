12d59178d95a0dccbed32bf1ed7ed4d56f1a42ee
ekanata14 <ekanata1411@gmail.com>
2024-12-01 09:26:25 +0800

    feat: plus string and escape character javascript

--- javascript/data-types/string.js
+++ javascript/data-types/string.js
@@ -17,3 +17,20 @@ document.writeln("Hello Dunia")
 document.writeln("<br>")
 
 document.writeln("Hello Lagi");
+
+// String (+)
+// String can be added by another string to combine them
+// Here's the example
+
+document.writeln("Nama: " + "dreamerdream");
+
+// String also have escape character start by "\"
+// These are the escape sequence that exist in javascript (\n, \t, \', \", \\)
+
+document.writeln("<textarea>")
+document.writeln("dreamer \n dream")
+document.writeln("dreamer \t catch")
+document.writeln("dreamer \t \"qoutes double\"")
+document.writeln("dreamer \t \'qoutes single\'")
+document.writeln("dreamer \t \\'qoutes backslash\\'")
+document.writeln("</textarea>")
