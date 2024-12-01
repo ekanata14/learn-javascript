a4da9dc1b05606c1ac086eef42ec5e12bf02b03d
ekanata14 <ekanata1411@gmail.com>
2024-12-01 10:00:20 +0800

    feat: learn javascript variable

---
+++ javascript/variable.js
@@ -0,0 +1,24 @@
+// Javascript variable
+// Javascript variable is a container that holds the value of the given data type.
+// It can be filled up with any data type in javascript. So it can be a flexible variable.
+// To access a variable, you can directly write the name of the variable in console.log or document.writeln or something that display or output a value
+
+// There are 3 types of javascript variable
+/**
+ * 1. var : Variable that can be redeclared and updated but it's deprecated due to the new update for the standarization
+ * 2. let : Variable that can be updated but can't redeclared
+ * 3. const: Variable that cant' be updated and redeclared
+ */
+
+// Here's the example of javascript variable
+
+const myName = "dreamerdream"
+let age = 20
+var isStudent = true // Deprecated one
+
+document.writeln(myName)
+document.writeln("<br>")
+document.writeln(age)
+document.writeln("<br>")
+document.writeln(isStudent)
+document.writeln("<br>")
